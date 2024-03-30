26a6e935ae746cb0a8b791ad6aa55c12ad5970aa
Yao Yao <lowsfer@gmail.com>
2024-03-30 19:42:20 +0800

    Add copyright header

--- Atomic.h
+++ Atomic.h
@@ -1,3 +1,20 @@
+/*
+Copyright [2024] [Yao Yao]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 // The purpose of this is to provide a atomic class that share lock among many objects.
 // The most useful case is that when there are many objects, the chance for lock contention
 // is low. It would be nice to make each mutex protect multiple objects. When contention is
